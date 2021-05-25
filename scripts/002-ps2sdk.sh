750dc8c12f965b3265db6a42f1b68ac4fe5d0897
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2021-05-25 10:36:33 +0300

    Added windows toolchain
    * Added MSYS/MINGW32 toolchain
    * Fixed Github Action to upload artifacts even if some toolchains failed to compile
    * Fixed some scripts to work with non-default branches
    * Changed generated toolchain naming scheme

--- scripts/002-ps2sdk.sh
+++ scripts/002-ps2sdk.sh
@@ -10,13 +10,13 @@ REPO_URL="https://github.com/ps2dev/ps2sdk"
 REPO_FOLDER="ps2sdk"
 
 # Checking if a specific TAG has been selected, it is passed using parameter $1
-[  -z "$1" ] && REPO_REFERENCE="origin/master" || REPO_REFERENCE=$1
+[  -z "$1" ] && REPO_REFERENCE="master" || REPO_REFERENCE=$1
 echo "Using repo reference $REPO_REFERENCE"
 
 if test ! -d "$REPO_FOLDER"; then
-	git clone $REPO_URL|| exit 1
+	git clone $REPO_URL -b ${REPO_REFERENCE} || exit 1
 fi
-cd $REPO_FOLDER && git fetch origin && git reset --hard ${REPO_REFERENCE} || exit 1
+cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${REPO_REFERENCE} && git checkout ${REPO_REFERENCE} || exit 1
 
 ## Determine the maximum number of processes that Make can work with.
 #OSVER=$(uname)
